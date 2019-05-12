88128d94d9786c3689ec068f1505617ed1ecfa22
wojciknatalia <nataliaaawojcik@gmail.com>
2019-05-12 15:42:14 +0200

    Added sliders

--- src/main/java/com/korp/cannon/GameLoop.java
+++ src/main/java/com/korp/cannon/GameLoop.java
@@ -1,22 +1,32 @@
 package com.korp.cannon;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class GameLoop extends Canvas{
+public class GameLoop extends Canvas implements MouseListener, MouseMotionListener {
 
     private boolean running = false;
     private final int FPS = 30;
 
     private LinkedList<Bullet> objects = new LinkedList<Bullet>();
     private Cannon cannon;
+    private Slider angleSlider=new Slider(50,80,157,0,"Angle");
+    private Slider sizeSlider=new Slider(50, 140, 0, 75, "Size");
+    private Slider powerSlider=new Slider(50, 200, 150, 0, "Power");
 
     public GameLoop(){
         new Window(800, 640, "Cannon Shooter", this);
         cannon = new Cannon(0,500, 50, 10);  //od 0 do -90
         addObject(new Test(0,500,5,-3));
+        addMouseListener(this);
+        addMouseMotionListener(this);
     }
 
 
@@ -48,13 +58,25 @@ public class GameLoop extends Canvas{
         }
 
         Graphics g = bs.getDrawGraphics();
+
+        //Background
         g.setColor(Color.pink);
         g.fillRect(0,0, 800, 640);
 
+        //Bullet
         for(Bullet obj: objects){
             obj.render(g);
         }
+
+        //Sliders
+        angleSlider.render(g);
+        sizeSlider.render(g);
+        powerSlider.render(g);
+
+        //Cannon
         cannon.render(g);
+
+
 
         g.dispose();
         bs.show();
@@ -94,4 +116,39 @@ public class GameLoop extends Canvas{
             }
         }
     }
+
+    //============ Mouse Listening ============
+    static boolean dragging = false;
+    static boolean click = false;
+    static int cursorX = 0;
+    static int cursorY = 0;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        click = true;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        dragging = true;
+        cursorX = e.getX();
+        cursorY = e.getY();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        dragging = false;
+        click = false;
+        cursorX = e.getX();
+        cursorY = e.getY();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mousePressed(MouseEvent e) {}
 }
---
+++ src/main/java/com/korp/cannon/Slider.java
@@ -0,0 +1,57 @@
+package com.korp.cannon;
+
+import java.awt.*;
+
+public class Slider {
+    private int min;
+    private int max;
+    private int x;
+    private int y;
+
+    private int width = 250;
+    private int height = 5;
+    private String label;
+    private int sliderX;
+    private int sliderWidth = 5;
+    private int sliderHeight = 20;
+    private boolean sliderGrapped = false;
+
+    public Slider(int x, int y, int min, int max, String label){
+        this.x=x;
+        this.y=y;
+        this.min=min;
+        this.max=max;
+        this.label=label;
+        this.sliderX=(width/2)-(sliderWidth/2);
+    }
+
+    public void render(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+
+        if (GameLoop.dragging && GameLoop.cursorX > (sliderX - 10) + x && GameLoop.cursorX < sliderX + (sliderWidth + 10) + x && GameLoop.cursorY > y && GameLoop.cursorY < y + height) {
+            sliderGrapped = true;
+        }
+        if (!GameLoop.dragging) {
+            sliderGrapped = false;
+        }
+        if (sliderGrapped && GameLoop.cursorX > x + (sliderWidth / 2) && GameLoop.cursorX < x + width - 1) {
+            sliderX = GameLoop.cursorX - x - (sliderWidth / 2);
+        }
+
+        g2.setColor(Color.GRAY);
+        g2.fillRect(x, y, width, height);
+
+        g2.setColor(Color.BLACK);
+        g2.fillRect(sliderX + x, y - (sliderHeight / 3), sliderWidth, sliderHeight);
+
+        g2.setColor(Color.BLACK);
+        Font f = new Font("Monospaced", Font.PLAIN, 20);
+        g2.setFont(f);
+        g2.drawString(label, (width / 2) - (sliderWidth / 2), y - 12);
+    }
+
+    public int getVal(){
+        return (int)((double)(sliderX+(sliderWidth/2))/(double)width*(double)(max-min));
+    }
+
+}
