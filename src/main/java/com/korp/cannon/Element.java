2ab32fcd635ea8526fe7380d2893e6ba72e8aa1b
Jakub Hejduk <karbonatyt@gmail.com>
2019-05-15 13:49:59 +0200

    add Element class

---
+++ src/main/java/com/korp/cannon/Element.java
@@ -0,0 +1,12 @@
+package com.korp.cannon;
+
+public abstract class Element {
+    public double x;
+    public double y;
+    public double vx;
+    public double vy;
+
+    private void update(){};
+    
+    private void render(){};
+}
